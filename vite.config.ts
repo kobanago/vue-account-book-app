1f929abbbe329ea34d712f3cf742bfaeb6746d59
kobanago <kobanago3800@gmail.com>
2024-05-01 09:54:26 +0900

    chore：tsconfigPath：fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), tsconfigPaths()],
+  plugins: [
+    vue(),
+    tsconfigPaths({
+      root: '../../',
+      loose: true,
+    }),
+  ],
 });
